b50ecd88c8ed0bc3a27fee7c5cfef6b5691e30c5
gray <gray@pipe.com>
2024-04-19 17:29:42 -0500

    save

---
+++ dbsetup.py
@@ -0,0 +1,16 @@
+import psycopg2
+
+def setup_database():
+    conn = psycopg2.connect("postgresql://postgres:password@localhost:5432")  # Update connection details
+    cursor = conn.cursor()
+    
+    cursor.execute('''
+        create table game_turns (
+            id bigint generated always as identity primary key,
+            turn_data jsonb
+        )
+    ''')
+    conn.commit()
+    conn.close()
+    
+setup_database()
